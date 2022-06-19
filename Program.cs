5cf4a700a130720e7144fb0bedfafcd7eac541c6
Naday <girlgroup@mail.ru>
2022-06-19 14:57:33 +0300

    DZ_

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿//Задача 1 показать большее и меньшее число
+
+// Console.WriteLine("Введите два числа");
+ //int num1= Convert.ToInt32(Console.ReadLine());
+ //int num2= Convert.ToInt32(Console.ReadLine());
+ 
+//if (num1 > num2){
+    
+//Console.WriteLine("max = " + num1);
+ // Console.WriteLine("min = " + num2);
+// }
+
+ //else{
+ // Console.WriteLine("max = " + num2);
+ //   Console.WriteLine("min = " + num1);
+ 
+// }
+
+// Задача 1 - решена 
+
+
+// Задача 2 найти максимальное из трех чисел
+
+// Console.WriteLine("Введите три числа");
+//  int num1= Convert.ToInt32(Console.ReadLine());
+//  int num2= Convert.ToInt32(Console.ReadLine());
+//  int num3= Convert.ToInt32(Console.ReadLine());
+//  int  max;
+//  if (num1 > num2){
+//  }
+//   if (num1 > num3){
+//      max=num1;
+// Console.WriteLine("max = " + num1);
+// }
+
+//   else
+//      max=num3;
+//   Console.WriteLine("max = " + num3);
+   
+//    if (num2 > num3){
+//    }
+
+//      if (num2 > num1){   
+//     max=num2;
+
+// Console.WriteLine("max = " + num2);
+//      }
+
+// else
+//   Console.WriteLine("max = " + num3);
+
+
+// Задача 2 выдает 2 мах числа(((
+
+
+ //Задача 3 принимает на вход число и выдает четное 
+
+ // Console.WriteLine("Введите число");
+//  int num= Convert.ToInt32(Console.ReadLine());
+  
+ //  if (num % 2 ==0 ){
+
+ //  Console.WriteLine("Четное число " + num);
+  // } 
+ //  else 
+  // Console.WriteLine("это нечетное число " + num);
+   
+  // Задача 3 решена !!!
+  
+
+  //Задача 4 на вход принимает число (N), 
+  //а на выходе показывает все чётные числа от 1 до N.
+
+
+ 
+  // Console.WriteLine("Write numbers ");
+  // int num = Convert.ToInt32(Console.ReadLine());
+  // int current;
+  // if (num >= 1 ){
+  // current = num;
+  // }
+  // else{
+  
+  //   num = ( num % 2 ==0);
+  
+  // Console.WriteLine("current " + "   " );
+  // }
